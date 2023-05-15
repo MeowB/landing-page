e71fc05de27ca0ff7465e6408dc5e16f34a497a8
MeowB <60074375+MeowB@users.noreply.github.com>
2023-05-15 05:02:20 +0200

    branch check

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,10 @@ export default function Header() {
 	return (
 		<header>
 			<Nav />
-
-			<h1>Data <span className="underlined">tailored</span> to your needs.</h1>
-			<Button value="Learn more"/>
+			<div className="content">
+				<h1>Data <span className="underlined">tailored</span> to your needs.</h1>
+				<Button value="Learn more"/>
+			</div>
 			<div className="image-container">
 				<img src="src\assets\images\image-hero.webp" alt="image of a phone" />
 			</div>
