b28178ff12ef47cae0060d5d330069d0edd2e3b3
MeowB <60074375+MeowB@users.noreply.github.com>
2023-05-22 10:35:41 +0200

    trying to render images on gh-page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import './styles/Nav.scss'
 export default function Nav() {
 	return (
 		<nav>
-			<img src="src\assets\images\logo-light.svg" alt="Logo" />
+			<img src=".\src\assets\images\logo-light.svg" alt="Logo" />
 			<Link source='#' value='Apply for access'/>
 		</nav>
 	)
